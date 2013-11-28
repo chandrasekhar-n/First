c6d2bc87d1a71d29549bb6a626e7bec9e934cdba
Skumar <skumarv@hotmail.com>
2013-11-28 23:27:19 +0530

    comitting by user 1 @ branch 1

--- src/pack/Testing.java
+++ src/pack/Testing.java
@@ -10,7 +10,7 @@ public class Testing {
 		Testing Testing = new Testing();
 		System.out.println("Change done by user 1 on branch 1 :: "+Testing.getClass().getName());
 		System.out.println("Changes by user 2 @ branch 1");
-		System.out.println("Changes by user 1 @ branch 1");
+		System.out.println("Change by user 1 @ branch 1");
 		System.out.println("Testing remote branch");
 		System.out.println("Conflict test by another user");
 		System.out.println("Testing for tag release");
