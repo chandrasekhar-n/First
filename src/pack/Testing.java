54995572cc0435c5f3e51b0fc0a72ed725212833
Skumar <skumarv@hotmail.com>
2013-12-02 07:32:46 +0530

    sync by user 1

--- src/pack/Testing.java
+++ src/pack/Testing.java
@@ -19,7 +19,7 @@ public class Testing {
 		System.out.println("change by goms");
 		System.out.println("Have done change here");
 		System.out.println("stagngh tes");
-		System.out.println("Syncby user 1 - test change");
+		System.out.println("Syncby user 1");
 	}
 
 }
