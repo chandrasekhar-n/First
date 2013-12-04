d486f523af46a0ad54ebffdacfefe26c256a85b5
Skumar <skumarv@hotmail.com>
2013-12-04 06:19:33 +0530

    commit change

--- src/pack/Testing.java
+++ src/pack/Testing.java
@@ -22,7 +22,7 @@ public class Testing {
 		System.out.println("stagngh tes");
 		System.out.println("Syncby user 1");
 		System.out.println("Change by user 1");
-		System.out.println("Change user 111");
+		System.out.println("Change user 1");
 	}
 
 }
